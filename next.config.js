4d7bd8a5eb9a6c8755845d86e126adc1385f852f
xieziji <xieziji9@gmail.com>
2025-10-30 11:01:00 +0800

    配置 GitHub Pages 部署

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/moyu-daily' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/moyu-daily/' : '',
+}
 
 module.exports = nextConfig
 
