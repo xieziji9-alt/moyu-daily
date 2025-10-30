b143b0d042305e6c1484338085803892ba79a750
xieziji <xieziji9@gmail.com>
2025-10-30 11:05:20 +0800

    恢复配置，准备使用 Vercel 部署

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/moyu-daily' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/moyu-daily/' : '',
-}
+const nextConfig = {}
 
 module.exports = nextConfig
 
